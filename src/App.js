9802093a5540df20065a763143d3084cee17d18f
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-19 06:41:02 -0800

    format app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
 
-function App() {
+/**
+ * //// Root Application ///////////////////////////////////////////////////////////
+ * @returns HTML for the Root Application
+ */
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
