0074940499a80fc59c162c35c5282c58dcfc45cb
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-19 12:20:50 -0800

    add Show Person compenint

--- src/components/ShowPersonComp.jsx
+++ src/components/ShowPersonComp.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 /**
  * //// SHOW PERSON COMPONENT ////////////////////////////////////////////////////
  * @returns HTML to Show a Person from SW API
  */
 const ShowPerson = () => {
-  return (
-    <div >
-        <h1>Show Person Component</h1>
-    </div>
-  );
+    // //// FIELDS ///////////////////////////////////////////////////////////////
+    const [person, setperson] = useState({});
+
+    // //// OUTPUT ///////////////////////////////////////////////////////////////
+    return (
+        <div >
+            <h1>Show Person Component</h1>
+            <p>Person: { JSON.stringify(person) }</p>
+        </div>
+    );
 };
 
 export default ShowPerson;
