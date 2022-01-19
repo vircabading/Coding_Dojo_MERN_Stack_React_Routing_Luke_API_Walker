1aeafd794329450569277a8177943ed8cf6220c6
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-19 15:52:28 -0800

    add planet name to ShowPerson

---
+++ src/components/PlanetBtnComp.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+
+/**
+ * //// PLANET BUTTON COMPONENT //////////////////////////////////////////////////
+ * @returns HTML button for a Planet from SW API
+ */
+const PlanetBtn = (props) => {
+    // //// FIELDS ///////////////////////////////////////////////////////////////
+    const [planet, setPlanet] = useState({});
+    const {  searchField} = props;
+    const history = useHistory();
+    console.log("Props searchField: ", JSON.stringify(props.searchField))
+    
+
+    useEffect(() => {
+        axios.get(searchField)
+            .then(response => {
+                console.log("Response data for planet BUTTON:", JSON.stringify(response.data));
+                setPlanet(response.data);
+            })
+            .catch(error => {
+                console.log("ERROR FOUND")
+            }
+
+            );
+    }, []);
+
+    // //// OUTPUT ///////////////////////////////////////////////////////////////
+    return (
+        <button className='btn btn-sm round btn-success'>{planet.name}</button>
+    );
+};
+
+export default PlanetBtn
--- src/components/ShowPersonComp.jsx
+++ src/components/ShowPersonComp.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, useHistory } from 'react-router-dom';
+import { Link, useParams, useHistory } from 'react-router-dom';
 import axios from 'axios';
 
 
@@ -10,6 +10,7 @@ import axios from 'axios';
 const ShowPerson = () => {
     // //// FIELDS ///////////////////////////////////////////////////////////////
     const [person, setperson] = useState({});
+    const [planet, setPlanet] = useState({});
     const { id } = useParams();
     const history = useHistory();
 
@@ -18,26 +19,32 @@ const ShowPerson = () => {
             .then(response => {
                 console.log("Response data:", JSON.stringify(response.data));
                 setperson(response.data);
+                axios.get(response.data.homeworld)
+                    .then(response => {
+                        console.log("Response homeworld data:", JSON.stringify(response.data));
+                        setPlanet(response.data);
+                    })
+                    .catch(error => {
+                        console.log("ERROR FOUND")
+                    });
             })
             .catch(error => {
                 console.log("ERROR FOUND")
                 history.push("/error");
-            }
-
-            );
-    } ,[]);
+            });
+    }, []);
 
     // //// OUTPUT ///////////////////////////////////////////////////////////////
     return (
         <div >
-            <h1>{ person.name }</h1>
-            <p>Person: { JSON.stringify(person) }</p>
+            <h1>{person.name}</h1>
+            <p>Person: {JSON.stringify(person)}</p>
             <hr></hr>
             <p>Height: {person.height}</p>
-            <p>Mass: { person.mass }</p>
-            <p>Hair Color: { person.hair_color }</p>
-            <p>Eye Color: { person.eye_color }</p>
-            <p>Home Planet: { person.homeworld }</p>
+            <p>Mass: {person.mass}</p>
+            <p>Hair Color: {person.hair_color}</p>
+            <p>Eye Color: {person.eye_color}</p>
+            <p>Planet: {planet.name}</p>
 
         </div>
     );
