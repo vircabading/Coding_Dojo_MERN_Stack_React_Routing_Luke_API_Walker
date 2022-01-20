a4c27c216e2eb8266500031b4b6e74e8e9db7b50
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-20 03:28:24 -0800

    add comments to error component

--- src/components/ErrorComp.jsx
+++ src/components/ErrorComp.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
-
+/**
+ * //// ERROR COMPONENT ////////////////////////////////////////
+ * @returns HTML for Error after query to the SW API
+ */
 const Error = () => {
+    // //// OUTPUT /////////////////////////////////////////////
     return (
         <div>
-            <img src='https://c.tenor.com/zZl0SFFuq7EAAAAC/kenobi-not-the-droids.gif' alt="Obi Won These arent the droids"></img>
+            {/* **** Image of Obi-Wan Kenobi ******** */}
+            <img src='https://c.tenor.com/zZl0SFFuq7EAAAAC/kenobi-not-the-droids.gif' 
+                    width="90%" 
+                    alt="Obi-Wan These arent the droids you're looking for">
+            </img>
         </div>);
 };
 
